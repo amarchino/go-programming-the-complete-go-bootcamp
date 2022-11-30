39b23e88d3e53e18acbfcb7e975f2d091bb868dc
amarchino <marchino.alessandro@gmail.com>
2022-11-30 22:39:29 +0000

    Lesson 018

---
+++ 02_go_basics/types_zero_values.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 4
+	var b = 5.2
+
+	a = int(b)
+	fmt.Println(a, b)
+
+	var value int
+	var price float64
+	var name string
+	var done bool
+
+	fmt.Println(value, price, name, done)
+}

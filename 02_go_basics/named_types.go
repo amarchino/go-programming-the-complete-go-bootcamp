5b20ba6eac7340d640862baa2d31f7ccfd21fb20
amarchino <marchino.alessandro@gmail.com>
2022-12-02 22:26:08 +0000

    Lesson 041

--- 02_go_basics/named_types.go
+++ 02_go_basics/named_types.go
@@ -1,8 +1,32 @@
 package main
 
+import "fmt"
+
+type km float64
+type mile float64
+
 func main() {
 	type age int
 	type oldAge int
 	type veryOldAge age
 
+	type speed uint
+	var s1 speed = 10
+	var s2 speed = 20
+
+	fmt.Println(s2 - s1)
+
+	var x uint
+	// x = s1
+	x = uint(s1)
+	_ = x
+
+	var s3 speed = speed(x)
+	_ = s3
+
+	var parisToLondon km = 465
+	var distanceInMiles mile
+
+	distanceInMiles = mile(parisToLondon) * 0.621
+	fmt.Println(distanceInMiles)
 }

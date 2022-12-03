88239eff44c03aa6a6fcec6581303b9db4762aff
amarchino <marchino.alessandro@gmail.com>
2022-12-03 20:10:49 +0000

    Lesson 060

--- 03_flow_control/for_continue_break.go
+++ 03_flow_control/for_continue_break.go
@@ -9,4 +9,16 @@ func main() {
 		}
 		fmt.Println(i)
 	}
+
+	count := 0
+	for i := 0; true; i++ {
+		if i%13 == 0 {
+			fmt.Printf("%d is divisible by 13\n", i)
+			count++
+		}
+		if count == 10 {
+			break
+		}
+	}
+	fmt.Println("Just a message after the loop")
 }

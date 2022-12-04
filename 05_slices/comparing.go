7666b39e1d6323cc98721c5aa8f242e30850e7b2
amarchino <marchino.alessandro@gmail.com>
2022-12-04 22:09:41 +0000

    Lesson 079

---
+++ 05_slices/comparing.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n []int
+	fmt.Println(n == nil)
+
+	m := []int{}
+	fmt.Println(m == nil)
+
+	a, b := []int{1, 2, 3}, []int{1, 2, 3}
+	// fmt.Println(a == b)
+
+	eq := true
+
+	a = nil
+	for i, valueA := range a {
+		if valueA != b[i] {
+			eq = false
+			break
+		}
+	}
+	if len(a) != len(b) {
+		eq = false
+	}
+
+	if eq {
+		fmt.Println("a and b slices are equal")
+	} else {
+		fmt.Println("a and b slices are not equal")
+	}
+}

21902e48da5d08cbcc4ad7bbb143ad65c5a188a3
amarchino <marchino.alessandro@gmail.com>
2022-12-29 22:09:13 +0000

    Lesson 145

---
+++ 11_pointers/data_types.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+func changeValues(quantity int, price float64, name string, sold bool) {
+	quantity = 3
+	price = 500.4
+	name = "Mobile Phone"
+	sold = false
+}
+func changeValuesByPointer(quantity *int, price *float64, name *string, sold *bool) {
+	*quantity = 3
+	*price = 500.4
+	*name = "Mobile Phone"
+	*sold = false
+}
+
+type Product struct {
+	price       float64
+	productName string
+}
+
+func changeProduct(p Product) {
+	p.price = 300
+	p.productName = "Bicycle"
+}
+func changeProductByPointer(p *Product) {
+	(*p).price = 300
+	p.productName = "Bicycle"
+}
+
+func changeSlice(s []int) {
+	for i := range s {
+		s[i]++
+	}
+}
+
+func changeMap(m map[string]int) {
+	m["a"] = 10
+	m["b"] = 20
+	m["c"] = 30
+}
+
+func main() {
+	quantity, price, name, sold := 5, 300.4, "Laptop", true
+	fmt.Println("Before calling changeValues():", quantity, price, name, sold)
+	changeValues(quantity, price, name, sold)
+	fmt.Println("After calling changeValues():", quantity, price, name, sold)
+
+	fmt.Println("Before calling changeValuesByPointer():", quantity, price, name, sold)
+	changeValuesByPointer(&quantity, &price, &name, &sold)
+	fmt.Println("After calling changeValuesByPointer():", quantity, price, name, sold)
+
+	fmt.Println()
+
+	gift := Product{
+		price:       100,
+		productName: "Watch",
+	}
+	fmt.Println("Before calling changeProduct():", gift)
+	changeProduct(gift)
+	fmt.Println("After calling changeProduct():", gift)
+
+	fmt.Println("Before calling changeProductByPointer():", gift)
+	changeProductByPointer(&gift)
+	fmt.Println("After calling changeProductByPointer():", gift)
+
+	fmt.Println()
+
+	prices := []int{1, 2, 3}
+	changeSlice(prices)
+	fmt.Println("After calling changeSlice():", prices)
+
+	myMap := map[string]int{"a": 1, "b": 2}
+	changeMap(myMap)
+	fmt.Println("After calling changeMap():", myMap)
+}

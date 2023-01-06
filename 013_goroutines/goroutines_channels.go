0e088af7a61c3804b12e7b95f5cd6c6387ed79ee
amarchino <marchino.alessandro@gmail.com>
2023-01-06 23:02:16 +0000

    Lesson 179

--- 013_goroutines/goroutines_channels.go
+++ 013_goroutines/goroutines_channels.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func factorial(n int, c chan int) {
 	f := 1
@@ -22,6 +25,19 @@ func main() {
 	for i := 1; i <= 20; i++ {
 		go factorial(i, ch)
 		f = <-ch
-		fmt.Println(f)
+		fmt.Println(i, f)
+	}
+
+	fmt.Println(strings.Repeat("#", 10))
+
+	for i := 5; i <= 15; i++ {
+		go func(n int, c chan int) {
+			f := 1
+			for i := 2; i <= n; i++ {
+				f *= i
+			}
+			c <- f
+		}(i, ch)
+		fmt.Printf("Factorial of %d is %d\n", i, <-ch)
 	}
 }

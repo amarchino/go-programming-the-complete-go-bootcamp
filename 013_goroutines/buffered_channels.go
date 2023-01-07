da1d1c7a33f88abf15e72cd6480aa1929d96c110
amarchino <marchino.alessandro@gmail.com>
2023-01-07 11:32:42 +0000

    Lesson 185

---
+++ 013_goroutines/buffered_channels.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan int, 3)
+
+	go func(c chan int) {
+		for i := 1; i <= 5; i++ {
+			fmt.Printf("func goroutine #%d starts sending data into the channel\n", i)
+			c <- i
+			fmt.Printf("func goroutine #%d after sending data into the channel\n", i)
+		}
+		close(c)
+	}(c)
+
+	fmt.Println("main goroutine sleeps for 2 seconds")
+	time.Sleep(time.Second * 2)
+
+	fmt.Println("main goroutine starts receiving data")
+	for v := range c {
+		fmt.Println("main goroutine received data:", v)
+	}
+
+	fmt.Println(<-c)
+	// c <- 10
+}

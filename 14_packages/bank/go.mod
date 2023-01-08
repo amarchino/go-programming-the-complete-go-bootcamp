fe8cfa8f652cb4cf95a076eb81a16583d3b691b1
amarchino <marchino.alessandro@gmail.com>
2023-01-08 22:07:39 +0000

    Lesson 192

---
+++ 14_packages/bank/go.mod
@@ -0,0 +1,5 @@
+module main
+
+require numbers v0.0.0
+
+replace numbers => ../numbers
---
+++ 14_packages/bank/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"numbers"
+)
+
+func main() {
+	fmt.Printf("%d is even: %t\n", 40, numbers.Even(40))
+}

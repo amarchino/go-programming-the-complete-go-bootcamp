2c4ad5b792bc0d97047eae9b885859a36d380a24
amarchino <marchino.alessandro@gmail.com>
2023-01-09 22:33:55 +0000

    Lesson 194

--- 14_packages/numbers/numbers.go
+++ 14_packages/numbers/numbers.go
@@ -1,5 +1,24 @@
 package numbers
 
+import "math"
+
 func Even(n uint) bool {
 	return n%2 == 0
+}
+
+func odd(n int) bool {
+	return n%2 != 0
+}
+
+func isPrime(num int) bool {
+	sqrt := int(math.Pow(float64(num), 0.5))
+	for i := 2; i < sqrt; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+func OddAndPrime(n int) bool {
+	return odd(n) && isPrime(n)
 }

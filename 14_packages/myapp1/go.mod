9cc45e7c31c20c230ef5cd337e04b6d933a2cffb
amarchino <marchino.alessandro@gmail.com>
2023-01-13 22:17:39 +0000

    Lesson 200

---
+++ 14_packages/myapp1/go.mod
@@ -0,0 +1,5 @@
+module myapp1
+
+go 1.19
+
+require github.com/amarchino/go-math v1.0.0 // indirect
---
+++ 14_packages/myapp1/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"github.com/amarchino/go-math/geometry"
+)
+
+func main() {
+	fmt.Println(geometry.CubeVolume(5))
+}
